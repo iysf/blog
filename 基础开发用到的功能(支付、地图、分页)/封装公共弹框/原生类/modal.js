e952b52e40e677beec3c5d6799e3dc2d2a1a719c
iysf <yangshaofengfe@gmail.com>
2017-09-16 15:23:42 +0800

    完成公共弹框封装

--- 基础开发用到的功能(支付、地图、分页)/封装公共弹框/原生类/modal.js
+++ 基础开发用到的功能(支付、地图、分页)/封装公共弹框/原生类/modal.js
@@ -1,15 +1,30 @@
 !(function(__global__){
-	
+
 var log = console.log.bind(console)
 
-function modal () {
+function modal ( text ) {
 
-	var oModal = document.createElement('div')
-	div.style = ""
+	var oModal   = document.createElement('div')
+	var oContent = document.createElement('div')
+
+	oModal.style = "position: fixed;left: 0;top:0;width: 100%;height: 100%;z-index: 99;background: rgba(0, 0, 0,.4)"
+
+	oContent.style = "position: absolute;left: 50%;top: 50%;width: 40%;transform: translate(-50%,-50%);z-index: 100;background: #fff; text-align:center;"
+
+	oContent.innerHTML = text
+
+	document.body.appendChild(oModal)
+	document.body.appendChild(oContent)
+	
+	oModal.onclick = function () {
+		document.body.removeChild(oModal)
+		document.body.removeChild(oContent)
+	}
 
 }
 
 modal.show = function () {
+
 	if ( arguments.length == 1 ) {
 
 		if( arguments instanceof Object ) {
@@ -19,10 +34,10 @@ modal.show = function () {
 			for ( var key in modalObj ) {
 
 				if ( key == 'text' ) {
-					// 具体处理逻辑
+
+					modal(modalObj[key])
+
 				}
-				log(key)
-				log(modalObj[key])
 
 			}
 
