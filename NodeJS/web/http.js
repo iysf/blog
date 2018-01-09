148603b7c24ee9dbf4f199d1b4af4980ef2eafab
iysf <yangshaofengfe@gmail.com>
2018-01-09 15:39:49 +0800

    新增Nodejs 代理webApi接口

---
+++ NodeJS/web/http.js
@@ -0,0 +1,34 @@
+var express = require('express'),
+        app = express(),
+    request = require('request')
+
+app.use(function (req, res, next) {
+  if (req.url.indexOf('/webapi') != -1) {
+    console.log("methods:", req.method)
+    console.log("req/url:", req.url)
+    if (req.method == 'GET') {
+      
+    } else if (req.method == 'POST') {
+
+    }
+    var requestApi = 'http://47.94.74.150:9060' + req.url.slice(7)
+    // var doubanApi = 'https://api.douban.com/v2/book/1220562'
+
+    var qs = {
+      params: {account: '15712804364'},
+    }
+    qs.params = JSON.stringify(qs.params)
+
+    request({url: requestApi, method: 'get', qs, json: true}, function (error, response, body) {
+      // console.log("server response:", response)
+      console.log("server body:", body)
+      res.send(body)
+    })
+
+  } else {
+    res.sendFile(__dirname + '/index.html')
+  }
+})
+
+
+app.listen(3000)
