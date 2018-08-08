6a976673be267074dae760b703a32a89bf4e4e03
iysf <yangshaofengfe@gmail.com>
2018-08-08 10:29:01 +0800

    update

--- NodeJS/爬虫/js/http.js
+++ NodeJS/爬虫/js/http.js
@@ -4,15 +4,22 @@ var fs = require('fs')
 // var url = 'http://m.soyoung.com/yuehui/product'
 
 // 【玻尿酸隆鼻】下单免费打车【海薇1ml】注射额头、太阳穴、苹果肌、下巴、面颊、鼻子
+// app机构、
+
+//
+
+
+// http://m.soyoung.com/normal/cpwap253775?_json=1
 
 request({
-  url: 'http://m.soyoung.com/yuehui/product?ajax=1&lver=0.0.0',
-  method: 'POST',
-  form: {
-    // 城市ID
-    // district_id
-    index: 499
-  },
+  // url: 'http://m.soyoung.com/yuehui/product?ajax=1&lver=0.0.0',
+  url: 'http://m.soyoung.com/normal/cpwap253775?_json=1',
+  method: 'GET',
+  // form: {
+  //   // 城市ID
+  //   // district_id
+  //   index: 499
+  // },
   headers: {
     'Content-Type': 'application/x-www-form-urlencoded'
   }
