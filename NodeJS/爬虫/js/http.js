397154a55c5a5d038cff0ead17e05c10e06bb81f
iysf <yangshaofengfe@gmail.com>
2018-08-17 09:33:05 +0800

    update

---
+++ NodeJS/爬虫/js/http.js
@@ -0,0 +1,14 @@
+var config = require('./config')
+var request = require('request')
+var fs = require('fs')
+var city = require('./config/city.js')
+var db = {
+  hospitalId: require('./config/json.js')
+}
+
+// console.log('hospitalId:', db.hospitalId)
+// db
+// var mongodb = require('mongodb')
+// var server = new mongodb.Server('127.0.0.1', 27017, {})
+// var client = new mongodb.Db('mydatabase', server, {w: 1})
+// var DB_URL = 'mongodb://127.0.0.1:27017'
---
+++ webpack/README.md
@@ -0,0 +1,5 @@
+## npm
+在安装一个要打包到生产环境的安装包时，你应该使用 npm install --save，如果你在安装一个用于开发环境的安装包（例如，linter, 测试库等），你应该使用 npm install --save-dev。请在 [npm 文档](https://docs.npmjs.com/cli/install) 中查找更多信息。
+
+
+## webpack
