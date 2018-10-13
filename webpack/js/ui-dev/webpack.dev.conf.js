9af32928d90da493fde4cbc927640aa6c533b2eb
iysf <yangshaofengfe@gmail.com>
2018-10-13 16:19:18 +0800

    update

---
+++ webpack/js/ui-dev/webpack.dev.conf.js
@@ -0,0 +1,36 @@
+'use strict'
+const config = require('./config')
+const path = require('path')
+const webpack = require('webpack')
+const webpackMerge = require('webpack-merge')
+const webpackBaseConf = require('./webpack.base.conf.js')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = webpackMerge(webpackBaseConf, {
+  context: __dirname,
+  mode: 'development',
+  entry: {
+    app: '../src/main.js'
+  },
+  output: {
+    filename: '[name].dev.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: '../src',
+    hot: true,
+    // quiet: true, // necessary for FriendlyErrorsPlugin
+    open: config.dev.autoOpenBrowser, // auto open browser
+    port: config.dev.host
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, '../src/index.html'),
+      filename: 'index.html',
+      inject: true
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+})
